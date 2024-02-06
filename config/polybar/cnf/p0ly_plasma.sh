824bc3faf75b504422042c9df6176e6011a75371
47z1lu7h <rdd87@live.com>
2024-02-06 12:22:11 +0100

    upd

--- config/polybar/cnf/p0ly_plasma.sh
+++ config/polybar/cnf/p0ly_plasma.sh
@@ -1,2 +1,4 @@
 #!/bin/bash
-sleep 4; polybar -q right  -c ~/.config/polybar/themes/plasma.ini & disown; polybar -q left -c ~/.config/polybar/themes/plasma.ini & disown;
+sleep 3; 
+polybar -q right  -c ~/.config/polybar/themes/plasma.ini & disown; 
+polybar -q left -c ~/.config/polybar/themes/plasma.ini & disown;
